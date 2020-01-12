48cbbd109e17d041f0ca52f779878aed94d4722b
Michael Nahum <USGrant63@gmail.com>
2020-01-12 16:46:29 -0500

    saving json results in variable

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let baseURL = 'https://api.themoviedb.org/3/';
 let APIKEY  = '56b548ffac684aba0fcfddb46a52bba5';
 
+let results;
+
 let getMovies = function () {
   let searchTerm = document.getElementById('title').value;
   let url = "".concat(baseURL, 'search/movie?api_key=', APIKEY, '&query=', searchTerm); 
     fetch(url)
     .then((result)=>result.json())
-    .then((data)=> console.log(data) )
+    .then(data => results = data )
+    .then(() => console.log(results))
 }
 
 // document.getElementById('output').innerHTML = JSON.stringify(data, null, 4)
