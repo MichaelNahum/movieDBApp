1a39574a54931b0a135cc7180a477251c448f524
Michael Nahum <USGrant63@gmail.com>
2020-01-12 19:10:09 -0500

    more changes

--- script.js
+++ script.js
@@ -14,31 +14,8 @@ let getMovies = function () {
 
 // document.getElementById('output').innerHTML = JSON.stringify(data, null, 4)
 
-document.getElementById('getMovies').addEventListener('click', getMovies);
-document.getElementById('getMovies').addEventListener('keypress', function(e){ 
-  if (e.key === 'Enter') {
-    getMovies()
-  }
-});
-
-function getPosts() { 
-    fetch('https://api.themoviedb.org/3/search/movie?api_key=56b548ffac684aba0fcfddb46a52bba5')
-    .then((res) => res.json())
-    .then((data) => {
-        let output = '<h2>Posts</h2>';
-        data.forEach((post) => {
-            output += `
-          <div>
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-          </div>
-            `;
-        });
-    document.getElementById('output').innerHTML = output;
-    })
-}
-
 function getImages() {
+  let searchTerm = document.getElementById('title').value;
   let url = "".concat(baseURL, 'configuration?api_key=', APIKEY); 
     fetch(url)
     .then((result)=>{
@@ -48,7 +25,16 @@ function getImages() {
       baseImageURL = data.images.secure_base_url;
       configData = data.images;
       console.log('config:', data);
+      getMovies(searchTerm);
 })}
+
+document.getElementById('getMovies').addEventListener('click', getMovies);
+document.getElementById('getMovies').addEventListener('keypress', function(e){ 
+  if (e.key === 'Enter') {
+    getMovies()
+  }
+});
+
 
 // "https://image.tmdb.org/t/p/"
 
